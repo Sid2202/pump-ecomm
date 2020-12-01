4e476fc3eb4e165d4bc74ade0cdbd361fe5e34d5
Sid Karthik <sa268592@gmail.com>
2020-12-02 00:12:10 +0530

    card template and initial lang toggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Input } from '@material-ui/core';
 // import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import axios from 'axios'
@@ -10,36 +10,54 @@ import Categorycard from './Categorycard';
 
 
 
-function App() {
-  
-  $('[lang="ka"]').hide();
+function App(props) {
 
-  const myFunctionka = () => {
-   
-    $('[lang="en"]').hide();
-    $('[lang="ka"]').show();
-  }
-  const myFunctionen = () =>{
-    $('[lang="ka"]').hide();
-    $('[lang="en"]').show();
+  const [lang, setLang] = useState('Kannada')
+
+  const handleTogggle = () => {
+    if (lang == 'English'){
+      setLang('Kannada')
+    }
+    else{
+      setLang('English')
+    }
   }
 
+  // useEffect(() => {
+    
+  //     var eng = document.getElementById("en");
+  //     var kan = document.getElementById("ka");
+
+  //     if (lang == 'English')
+  //     {
+  //       eng.style.display = "block";
+  //       kan.style.display = "none";
+  //     }
+
+  //     else
+  //     {
+  //       eng.style.display = "none";
+  //       kan.style.display = "block";
+  //     }
+
+
+  // }, [lang])
   return (
     <div className="App">
       <div className="productmain">
         <div className="mainheading">
           <h4>
-            <strong lang="en">Water Pump Selection</strong>
-            <strong lang="ka">ನೀರಿನ ಪಂಪ್ ಆಯ್ಕೆ</strong>
+            <strong lang="en" id="en">Water Pump Selection</strong>
+            <strong lang="ka" id="ka">ನೀರಿನ ಪಂಪ್ ಆಯ್ಕೆ</strong>
           </h4>
-          <button id="switch-lang" onClick="myFunctionka">Switch Language</button>
+          <button id="switch-lang" onClick={handleTogggle} >Switch Language</button>
         </div>
 
         <div className="container">
           <div className="categoryinfo">
             <div className="category_heading">
-              <h5 lang="en">For Domestic and Community</h5>
-              <h5 lang="ka">ಗೃಹ ಮತ್ತು ವಸತಿ ನಿಲಯಕ್ಕೆ</h5>
+              <h5 lang="en" id="en">For Domestic and Community</h5>
+              <h5 lang="ka" id="ka">ಗೃಹ ಮತ್ತು ವಸತಿ ನಿಲಯಕ್ಕೆ</h5>
             </div>
 
             <Categorycard />
@@ -49,72 +67,72 @@ function App() {
 
 
             {/* <div className="list-group">
-              <a href="soborewell.js" className="list-group-item" lang="en">
+              <a href="soborewell.js" className="list-group-item" lang="en" id="en">
                 Sump and Open Borewell
               </a>
-              <a href="soborewell.js" className="list-group-item" lang="ka">
+              <a href="soborewell.js" className="list-group-item" lang="ka" id="ka">
                 ತೊಟ್ಟಿ ಮತ್ತು ತೆರೆದ ಭಾವಿ
               </a>
-              <a href="dcborewell.js" className="list-group-item" lang="en">
+              <a href="dcborewell.js" className="list-group-item" lang="en" id="en">
                 Bore Well
               </a>
-              <a href="dcborewell.js" className="list-group-item" lang="ka">
+              <a href="dcborewell.js" className="list-group-item" lang="ka" id="ka">
                 ಕೊಳವೆ ಭಾವಿ
               </a>
             </div> */}
           </div>
-          <div className="categoryinfo">
+          {/* <div className="categoryinfo">
             <div className="category_heading">
-              <h5 lang="en">For Agriculture</h5>
-              <h5 lang="ka">ವ್ಯವಸಾಯಕ್ಕೆ</h5>
+              <h5 lang="en" id="en">For Agriculture</h5>
+              <h5 lang="ka" id="ka">ವ್ಯವಸಾಯಕ್ಕೆ</h5>
             </div>
             <div className="list-group">
-              <a href="aowell.js" className="list-group-item" lang="en">Open Well</a>
-              <a href="aowell.js" className="list-group-item" lang="ka">
+              <a href="aowell.js" className="list-group-item" lang="en" id="en">Open Well</a>
+              <a href="aowell.js" className="list-group-item" lang="ka" id="ka">
                 ತೆರೆದ ಭಾವಿ
               </a>
-              <a href="agriborewell.js" className="list-group-item" lang="en">
+              <a href="agriborewell.js" className="list-group-item" lang="en" id="en">
                 Bore Well
               </a>
-              <a href="agriborewell.js" className="list-group-item" lang="ka">
+              <a href="agriborewell.js" className="list-group-item" lang="ka" id="ka">
                 ಕೊಳವೆ ಭಾವಿ
               </a>
-              <a href="acorrp.js" className="list-group-item" lang="en">
+              <a href="acorrp.js" className="list-group-item" lang="en" id="en">
                 Canal or River
               </a>
-              <a href="acorrp.js" className="list-group-item" lang="ka">
+              <a href="acorrp.js" className="list-group-item" lang="ka" id="ka">
                 ಕಾಲುವೆ ಅಥವಾ ನದಿ
               </a>
-              <a href="alip.js" className="list-group-item" lang="en">
+              <a href="alip.js" className="list-group-item" lang="en" id="en">
                 Lift Irrigation
               </a>
-              <a href="alip.js" className="list-group-item" lang="ka">ಏತ್ ನೀರಾವರಿ</a>
-              <a href="agrisol.js" className="list-group-item" lang="en">
+              <a href="alip.js" className="list-group-item" lang="ka" id="ka">ಏತ್ ನೀರಾವರಿ</a>
+              <a href="agrisol.js" className="list-group-item" lang="en" id="en">
                 Solar Pumps
               </a>
-              <a href="agrisol.js" className="list-group-item" lang="ka">
+              <a href="agrisol.js" className="list-group-item" lang="ka" id="ka">
                 ಸೌರಶಕ್ತಿ ಪಂಪುಗಳು
               </a>
             </div>
           </div>
           <div className="categoryinfo">
             <div className="category_heading">
-              <h5 lang="en">For Industry</h5>
-              <h5 lang="ka">ಕೈಗಾರಿಕೆಗೆ</h5>
+              <h5 lang="en" id="en">For Industry</h5>
+              <h5 lang="ka" id="ka">ಕೈಗಾರಿಕೆಗೆ</h5>
             </div>
             <div className="list-group">
-              <a href="indpol.js" className="list-group-item" lang="en">Polder</a>
-              <a href="indpol.js" className="list-group-item" lang="ka">
+              <a href="indpol.js" className="list-group-item" lang="en" id="en">Polder</a>
+              <a href="indpol.js" className="list-group-item" lang="ka" id="ka">
                 ತೊಟ್ಟಿ ಅಥವಾ ಹೊಂಡದ (ಕೊಳ)
               </a>
-              <a href="#" className="list-group-item" lang="en">
+              <a href="#" className="list-group-item" lang="en" id="en">
                 Special Purpose Pumps
               </a>
-              <a href="#" className="list-group-item" lang="ka">
+              <a href="#" className="list-group-item" lang="ka" id="ka">
                 ವಿಶೇಷ ಉದ್ದೇಶದ ಪಂಪುಗಳು
               </a>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
--- src/soborewell.js
+++ src/soborewell.js
@@ -36,16 +36,16 @@ function soborewell() {
     .catch(err => console.log(err))
   }
 
-  $('[lang="ka"]').hide();
+  $('[lang="ka" id="ka"]').hide();
 
 const myFunctionka = () => {
  
-  $('[lang="en"]').hide();
-  $('[lang="ka"]').show();
+  $('[lang="en" id="en"]').hide();
+  $('[lang="ka" id="ka"]').show();
 }
 const myFunctionen = () =>{
-  $('[lang="ka"]').hide();
-  $('[lang="en"]').show();
+  $('[lang="ka" id="ka"]').hide();
+  $('[lang="en" id="en"]').show();
 }
 
 
@@ -69,8 +69,8 @@ const myFunctionen = () =>{
              
           </div>
         <h5>
-          <span lang="en">Domestic Sump / Open Well Pump Selection</span>
-          <span lang="ka">
+          <span lang="en" id="en">Domestic Sump / Open Well Pump Selection</span>
+          <span lang="ka" id="ka">
             ಗೃಹ ಮತ್ತು ವಸತಿ ನಿಲಯಕ್ಕೆ ತೊಟ್ಟಿ / ತೆರೆದ ಭಾವಿ ಪಂಪಿನ ಆಯ್ಕೆ
           </span>
         </h5>
@@ -81,29 +81,29 @@ const myFunctionen = () =>{
         <form action="soborewell.php">
           <div className="card">
             <div className="forminput">
-              <h5 lang="ka">ನೀರಿನ ಅವಶ್ಯಕತೆ ದಿನಕ್ಕೆ ಲೀಟರ್ಗಳಲ್ಲಿ</h5>
-              <h5 lang="en">Total Water Required per day</h5>
+              <h5 lang="ka" id="ka">ನೀರಿನ ಅವಶ್ಯಕತೆ ದಿನಕ್ಕೆ ಲೀಟರ್ಗಳಲ್ಲಿ</h5>
+              <h5 lang="en" id="en">Total Water Required per day</h5>
               &emsp;
               <div className="input-suffix">
                 <input type="number"   className="" name="totalwater" onChange={async (e) => setwaterreq(e.target.value)} />
                 <h6>
-                  <strong lang="en">in litres</strong>
-                  <strong lang="ka">ಲೀಟರ್ಗಳಲ್ಲಿ</strong>
+                  <strong lang="en" id="en">in litres</strong>
+                  <strong lang="ka" id="ka">ಲೀಟರ್ಗಳಲ್ಲಿ</strong>
                 </h6>
               </div>
             </div>
 
             <div className="forminput">
-              <h5 lang="ka">
+              <h5 lang="ka" id="ka">
                 ಒಟ್ಟು ಎಷ್ಟು ಎತ್ತರಕ್ಕೆ ನೀರನ್ನು ಎತ್ತಬೇಕು ಮೀಟರ್ಗಳಲ್ಲಿ
               </h5>
-              <h5 lang="en">Total Height to be pumped</h5>
+              <h5 lang="en" id="en">Total Height to be pumped</h5>
               &emsp;
               <div className="input-suffix" >
                 <input type="number" className="" onChange={async (e) => setHeight(e.target.value)}/>
                 <h6 >
-                  <strong lang="en">in metres</strong>
-                  <strong lang="ka">ಮೀಟರ್ಗಳಲ್ಲಿ</strong>
+                  <strong lang="en" id="en">in metres</strong>
+                  <strong lang="ka" id="ka">ಮೀಟರ್ಗಳಲ್ಲಿ</strong>
                 </h6>
               </div>
             </div>
@@ -111,29 +111,29 @@ const myFunctionen = () =>{
             
           <div className="card">
             <div className="formradio">
-              <h4 lang="ka">ನೀರಿನ ಅವಶ್ಯಕತೆ ದಿನಕ್ಕೆ ಲೀಟರ್ಗಳಲ್ಲಿ</h4>
-              <h4 lang="en" >Power Supply available</h4>
+              <h4 lang="ka" id="ka">ನೀರಿನ ಅವಶ್ಯಕತೆ ದಿನಕ್ಕೆ ಲೀಟರ್ಗಳಲ್ಲಿ</h4>
+              <h4 lang="en" id="en" >Power Supply available</h4>
               <h6 >*Select Any One</h6>
               &emsp;
 
               <div className="radioinput">
-                <span lang="ka">ಒಂದು ಫೇಸ್ ೨೩೦ ವೋಲ್ಟ್</span>
-                <span lang="en">Single Phase 230V</span>
+                <span lang="ka" id="ka">ಒಂದು ಫೇಸ್ ೨೩೦ ವೋಲ್ಟ್</span>
+                <span lang="en" id="en">Single Phase 230V</span>
                 <input type="radio" name="power" id="rdbtn1" value="230" onChange={e => setPowsup(e.target.value)} />
               </div>
 
               <div className="radioinput">
-                <span lang="ka">ಮೂರು ಫೇಸ್ ೪೧೫ ವೋಲ್ಟ್</span>
-                <span lang="en">Three Phase 415V</span>
+                <span lang="ka" id="ka">ಮೂರು ಫೇಸ್ ೪೧೫ ವೋಲ್ಟ್</span>
+                <span lang="en" id="en">Three Phase 415V</span>
                 <input type="radio" name="power" value="415" onChange={e => setPowsup(e.target.value)}/>
               </div>
 
             </div>
             <div className="formwaterpump" >
-              <h5 lang="ka">
+              <h5 lang="ka" id="ka">
                 ಒಟ್ಟು ಎಷ್ಟು ಎತ್ತರಕ್ಕೆ ನೀರನ್ನು ಎತ್ತಬೇಕು ಮೀಟರ್ಗಳಲ್ಲಿ
               </h5>
-              <h5 lang="en">Which Make Pump Required</h5>
+              <h5 lang="en" id="en">Which Make Pump Required</h5>
               &emsp;
               <select type="number" className="" onSelect={(e) => setPumpmake(e.target.value)} defaultValue={"Any Make"}>
                 <option value="Any Make">Any Make</option>
@@ -144,15 +144,15 @@ const myFunctionen = () =>{
           
 
           <div className="buttons">
-            <Button type="button" lang="en" className="btn btn-light" id="clearbtn">
+            <Button type="button" lang="en" id="en" className="btn btn-light" id="clearbtn">
               CLEAR
             </Button>
-            <Button type="button" lang="ka" className="btn btn-light" id="clearbtn">
+            <Button type="button" lang="ka" id="ka" className="btn btn-light" id="clearbtn">
               ಅಳಿಸಿಹಾಕು
             </Button>
             <Button
               type="submit"
-              lang="en"
+              lang="en" id="en"
               className="btn btn-light"
               id="searchbtn"
               name=""
@@ -163,7 +163,7 @@ const myFunctionen = () =>{
             </Button>
             <Button
               type="submit"
-              lang="ka"
+              lang="ka" id="ka"
               className="btn btn-light"
               id="searchbtn"
               onClick={getsubmit}
